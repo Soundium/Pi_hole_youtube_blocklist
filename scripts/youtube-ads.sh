084f49f550aade39c76d180cc5cca98def584c97
Soundium <43785888+Soundium@users.noreply.github.com>
2020-03-30 00:02:06 +0200

    Update youtube-ads.sh

--- scripts/youtube-ads.sh
+++ scripts/youtube-ads.sh
@@ -1,3 +1,10 @@
+#!/bin/sh
+# This script will fetch the Googlevideo ad domains and append them to the Pi-hole block list.
+# Run this script daily with a cron job.
+# Project homepage: https://github.com/anudeepND/whitelist
+# Licence: https://github.com/Soundium/Pi_hole_youtube_blocklist/blob/master/LICENSE
+# Created by Soundium
+
 echo off
 
 #copy file to blacklist
