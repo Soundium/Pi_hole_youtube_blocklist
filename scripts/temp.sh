eb5f4345df9f5fe63b49d2662f5a51a042c449d5
Soundium <43785888+Soundium@users.noreply.github.com>
2020-03-29 23:59:58 +0200

    Create temp.sh

---
+++ scripts/temp.sh
@@ -0,0 +1,41 @@
+#!/bin/sh
+# This script will fetch the Googlevideo ad domains and append them to the Pi-hole block list.
+# Run this script daily with a cron job.
+# Project homepage: https://github.com/anudeepND/whitelist
+# Licence: https://github.com/Soundium/Pi_hole_youtube_blocklist/blob/master/LICENSE
+# Created by Soundium
+
+# File to store the YT ad domains
+FILE=/etc/temp/temp.txt
+
+# Wolfram Alfa APPID
+APPID="Register on https://www.wolframalpha.com/ and put your APPID here"
+POD_TIMEOUT="20"
+
+# Fetch the list of domains, remove the ip's and save them
+curl 'https://api.hackertarget.com/hostsearch/?q=googlevideo.com' \
+| awk -F, 'NR>1{print $1}' \
+| grep -vE "redirector|manifest" > $FILE
+
+# Replace r*.sn*.googlevideo.com URLs to r*---sn-*.googlevideo.com
+# and add those to the list too
+curl "http://api.wolframalpha.com/v2/query?input=googlevideo.com&appid=${APPID}&format=plaintext&podtimeout=${POD_TIMEOUT}&podstate=WebSiteStatisticsPod:InternetData__Subdomains&podstate=WebSiteStatisticsPod:InternetData__Subdomains_More” \
+
+| grep -Po "r\d+---sn-.+.googlevideo.com" >> $FILE
+
+# Scan log file for previously accessed domains
+grep r*.googlevideo.com /var/log/pihole.log \
+| awk '{print $6}' \
+| grep -v '^googlevideo.com\|redirector\|manifest' \
+| sort | uniq >> $FILE
+
+# Replace r*.sn*.googlevideo.com URLs to r*---sn-*.googlevideo.com
+# and add those to the list too
+cat $FILE | sed -i $FILE -re 's/(^r[[:digit:]]+)(\.)(sn)/\1---\3-/' >> $FILE
+
+#delete duplicates
+perl -i -ne 'print if ! $x{$_}++' /etc/temp/temp.txt
+
+#demonstrative
+cat $FILE >>  /var/www/html/temp.txt
+
